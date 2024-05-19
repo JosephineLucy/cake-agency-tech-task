1df71512dc6bf16f8e4273332066b8245343454f
josie <josephine.lucyrita@gmail.com>
2024-05-19 17:08:58 +0100

    removes footer and changes app background colour

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import "./styles.scss";
 import Header from "./components/header";
-import Footer from "./components/footer";
 import AverageCalculator from "./components/average-calculator";
 
 import { appConstants } from "./constants/constants";
+import ErrorBoundary from "./components/error-boundary";
 
 function App() {
   return (
     <>
       <Header title={appConstants.title} />
-      <AverageCalculator />
-      <Footer title={appConstants.title} />
+      <ErrorBoundary>
+        <AverageCalculator />
+      </ErrorBoundary>
     </>
   );
 }
