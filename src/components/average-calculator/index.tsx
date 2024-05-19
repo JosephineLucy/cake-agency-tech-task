957650e6dc0e8a1fc103c952c83cdd36f47813ae
josie <josephine.lucyrita@gmail.com>
2024-05-19 17:19:43 +0100

    removes loader

--- src/components/average-calculator/index.tsx
+++ src/components/average-calculator/index.tsx
@@ -3,31 +3,24 @@ import { calculateOrderAverage, fetchOrders } from "./helpers/helpers";
 import "./styles.scss";
 import AverageValue from "../average-value";
 import ErrorMessage from "../error-boundary/ErrorMessage";
-import Loader from "../loader";
 
 const AverageCalculator = () => {
   const [orderAverage, setOrderAverage] = useState<string>("");
   const [error, setError] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const onClick = () => {
     setError(false);
-    setIsLoading(true);
     fetchOrders()
       .then((orders) => {
         if (orders) {
           const orderAverage: string = calculateOrderAverage(orders);
           setOrderAverage(orderAverage);
-          setIsLoading(false);
         }
       })
       .catch((err) => {
         setError(err);
-        setIsLoading(false);
       });
   };
-
-  if (isLoading) return <Loader />;
 
   if (error) return <ErrorMessage />;
 
--- src/components/loader/index.tsx
+++
@@ -1,5 +0,0 @@
-const Loader = () => {
-  return <h4 style={{ color: "white" }}>Loading, please wait...</h4>;
-};
-
-export default Loader;
