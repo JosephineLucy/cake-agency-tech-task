b87b9bb9ac2b7a2461667a52e65228a22196d740
josie <josephine.lucyrita@gmail.com>
2024-05-28 15:53:08 +0100

    moves api endpoint into .env file

--- src/proxy-server/server.js
+++ src/proxy-server/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const app = express();
 
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  const response = await fetch("https://fauxdata.codelayer.io/api/orders");
+  const response = await fetch(process.env.API_ENDPOINT);
   const data = await response.json();
   res.json(await data);
 });
