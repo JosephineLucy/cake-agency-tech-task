623e9a9e6f67cb3f52f8dff56f617f72baab6e98
josie <josephine.lucyrita@gmail.com>
2024-05-28 16:36:18 +0100

    converts server file js -> ts

--- src/proxy-server/server.ts
+++ src/proxy-server/server.ts
@@ -3,13 +3,14 @@ import cors from "cors";
 import { configDotenv } from "dotenv";
 
 configDotenv();
+const endpoint: URL = new URL(process.env.API_ENDPOINT || "");
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  const response = await fetch(process.env.API_ENDPOINT);
+app.get("/", async (_req, res) => {
+  const response = await fetch(endpoint);
   const data = await response.json();
   res.json(await data);
 });
